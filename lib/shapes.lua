14e5afd14d3b6bfc46c978821ae895e216b52378
Jonathan Maw <jonathan.maw@gmail.com>
2012-12-27 19:58:23 +0000

    Add handling for storage of shapes

---
+++ lib/shapes.lua
@@ -0,0 +1,30 @@
+local shapes = {}
+
+local store = {}
+store.__mode = "v"
+
+function shapes.newcircle(radius)
+	local name = "circle"..radius
+	if not store[name] then
+		store[name] = love.physics.newCircleShape(radius)
+	end
+	return store[name]
+end
+
+function shapes.newring(radius)
+	local name = "ring"..radius
+	if not store[name] then
+		local segmentcount = math.max(10, math.floor(radius))
+		local i
+		local mult = 2 * math.pi / segmentcount
+		local chain_args = {}
+		for i=1,segmentcount do 
+			local angle = i * mult
+			table.insert(chain_args, radius * cos(angle))
+			table.insert(chain_args, radius * sin(angle))
+		end
+		store[name] = love.physics.newChainShape(true, unpack(chain_args))
+	end
+	return store[name]
+
+return shapes
