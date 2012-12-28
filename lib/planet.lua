07308b95c902ca4721d677fda4b6bb1aa543427e
Richard Maw <richard.maw@gmail.com>
2012-12-28 18:18:52 +0000

    Move updateBody to after fixture creation
    Adding the fixture resets the physics, so set the physics in
    updateBody after the fixture has been added.

--- lib/planet.lua
+++ lib/planet.lua
@@ -3,10 +3,10 @@ local Shapes = require "lib/shapes"
 local Actor = require "lib/actor"
 local Planet = Class{name = "Planet", inherits=Actor, function(self, t)
 	Actor.construct(self, t)
-	self:updateBody()
 	-- Fixture
 	local shape = Shapes.newring(t.radius or self.radius)
 	self.physfix = love.physics.newFixture(self.physbody, shape)
+	self:updateBody()
 	print("planet should have mass "..self.mass)
 	print("physbody is", self.physbody)
 	print("planet has mass "..self.physbody:getMass())
