5b3fa1ffea222b78beb5e922e313047c0b553d83
Jonathan Maw <jonathan.maw@gmail.com>
2013-01-05 21:35:01 +0000

    Planet is drawn by repetition instead of stretch

--- lib/planet.lua
+++ lib/planet.lua
@@ -27,6 +27,7 @@ Planet.angdamp = 1
 -- Planet.yoff has no default
 -- Planet.imgsize has no default
 Planet.imagepath = "assets/noise.png"
+-- Planet.spritebatch
 -- Planet.stencil has no default
 Planet.col = {0, 191, 0}
 
@@ -34,13 +35,28 @@ function Planet.initImage(self, imagepath)
 	self.image = Images.new(imagepath)
 	local w = self.image:getWidth()
 	local h = self.image:getHeight()
-	self.xoff = w / 2
-	self.yoff = h / 2
 	self.imgsize = w
 	self.stencil = love.graphics.newStencil(function()
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.circle("fill", 0, 0, self.radius)
 	end)
+	-- create the spritebatch to use as the drawable much like an
+	-- image.
+	local xsteps = math.ceil(2 * self.radius / w)
+	local ysteps = math.ceil(2 * self.radius / h)
+	local spritecount = (xsteps + 1) * (ysteps + 1)
+	self.spritebatch = love.graphics.newSpriteBatch(self.image,
+	                                                spritecount)
+	self.spritebatch:bind()
+	local x, y
+	for x = 0, xsteps do
+		for y = 0, ysteps do
+			self.spritebatch:add(x * w, y * h)
+		end
+	end
+	self.spritebatch:unbind()
+	self.xoff = self.radius
+	self.yoff = self.radius
 end
 
 function Planet.draw(self)
@@ -49,10 +65,10 @@ function Planet.draw(self)
 	local scale = 2 * self.radius / self.imgsize
 	love.graphics.push()
 	love.graphics.translate(posx, posy)
-	love.graphics.rotate(angle)
+	love.graphics.rotate(self.angle)
 	love.graphics.setStencil(self.stencil)
 	love.graphics.setColor(self.col)
-	love.graphics.draw(self.image, 0, 0, 0, scale, scale,
+	love.graphics.draw(self.spritebatch, 0, 0, 0, 1, 1,
 	                    self.xoff, self.yoff)
 	love.graphics.setStencil()
 	love.graphics.pop()
