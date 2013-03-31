508f39583c404ae752764d9cc4635a64684e2a88
Jonathan Maw <jonathan.maw@gmail.com>
2013-03-31 18:41:44 +0100

    Add more drawing options to the Building class
    Can define wallcol, bodycol, and imagepath

--- lib/building.lua
+++ lib/building.lua
@@ -13,17 +13,25 @@ local Building = Class{name="Building", inherits=Actor, function(self, t)
 	self.physfix = love.physics.newFixture(self.physbody, shape)
 
 	self.imagepath = t.imagepath
+	self:initImage(self.imagepath)
 end}
 
 --Defaults
 Building.radius = 16
--- Human.imagepath = "assets/human.png"
--- Human.image has no default
--- Human.headquad has no default
--- Human.xoff
--- Human.yoff
+Building.imagepath = "assets/warningbuilding.png"
+Building.wallcol = {63, 63, 63}
+Building.bodycol = {127, 127, 127}
+-- Building.image has no default
+-- Building.xoff has no default
+-- Building.yoff has no default
 Building.bodytype = "static"
 -- Methods
+
+function Building.initImage(self, imagepath)
+	self.image = Images.new(imagepath)
+	self.xoff = -self.image:getWidth() / 2
+	self.yoff = -self.image:getHeight() / 2
+end
 
 function Building.draw(self)
 	local posx, posy = self.physbody:getPosition()
@@ -31,9 +39,14 @@ function Building.draw(self)
 	love.graphics.push()
 	love.graphics.translate(posx, posy)
 	love.graphics.rotate(angle)
-	love.graphics.setColor(255, 255, 255)
+	love.graphics.setColor(self.bodycol)
 	love.graphics.rectangle("fill", -self.radius, -self.radius,
 	                        2 * self.radius, 2 * self.radius)
+	love.graphics.setColor(self.wallcol)
+	love.graphics.rectangle("line", -self.radius, -self.radius,
+	                        2 * self.radius, 2 * self.radius)
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.draw(self.image, self.xoff, self.yoff)
 	love.graphics.pop()
 end
 
